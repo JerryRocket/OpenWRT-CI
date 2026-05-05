7a85a9cb200b555e4107677f91fba5aa4f683bff
JerryRocket <50605801+JerryRocket@users.noreply.github.com>
2026-05-05 16:58:26 +0800

    Comment out homeproxy package update
    Comment out the homeproxy package update line in Packages.sh.

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -52,7 +52,7 @@ UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 #UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
 #UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 
-UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "master"
+#UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "master"
 #UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 #UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 #UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
